c4d7fe8876cd9e8861578d81b38a730fa64b423f
InovanRazvan <r.inovan@yahoo.com>
2018-12-12 19:44:31 +0200

    adaugat

---
+++ remote1.js
@@ -0,0 +1,17 @@
+/*function multiply(a,b){
+return a*b;
+}
+function square(n){
+return multiply(n,m);
+}
+function printSquare(n){
+var squared=square(n);
+console.log(squared);
+}
+printSquare(4);
+*/
+console.log("Hello");
+setTimeout(function(){
+	console.log('I am a callback');
+	},5000);
+	console.log("World");
